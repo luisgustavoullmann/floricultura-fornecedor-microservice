9b9f7fe151266022b2552f406556dff4fac83aec
luisgustavoullmann <luisgustavoullmann@gmail.com>
2020-06-28 11:34:04 -0300

    add logs

--- src/main/java/com/microservice/fornecedor/controller/InfoController.java
+++ src/main/java/com/microservice/fornecedor/controller/InfoController.java
@@ -2,6 +2,7 @@ package com.microservice.fornecedor.controller;
 
 import com.microservice.fornecedor.model.InfoFornecedor;
 import com.microservice.fornecedor.service.InfoService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * Created by Luis Gustavo Ullmann on 26/06/2020
  */
+@Slf4j
 @RestController
 @RequestMapping("/info")
 public class InfoController {
@@ -20,6 +22,7 @@ public class InfoController {
 
     @GetMapping("/{estado}")
     public InfoFornecedor getInfoPorEstado(@PathVariable("estado") String estado){
+        log.info("Recebido o pedido de informações do fornecedor de {}", estado);
         return infoService.getInfoPorEstado(estado);
     }
 }
--- src/main/java/com/microservice/fornecedor/controller/PedidoController.java
+++ src/main/java/com/microservice/fornecedor/controller/PedidoController.java
@@ -3,6 +3,7 @@ package com.microservice.fornecedor.controller;
 import com.microservice.fornecedor.model.Pedido;
 import com.microservice.fornecedor.model.dto.ItemPedidoDto;
 import com.microservice.fornecedor.service.PedidoService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,6 +12,7 @@ import java.util.List;
 /**
  * Created by Luis Gustavo Ullmann on 27/06/2020
  */
+@Slf4j
 @RestController
 @RequestMapping("pedido")
 public class PedidoController {
@@ -25,6 +27,7 @@ public class PedidoController {
 
     @PostMapping
     public Pedido realizaPedido(@RequestBody List<ItemPedidoDto> produtos){
+        log.info("Fornecedor já está com o pedido");
         return pedidoService.realizaPedido(produtos);
     }
 }
